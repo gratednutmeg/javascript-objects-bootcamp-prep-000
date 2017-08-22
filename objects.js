b67c7d404a6393b039fdc632daade35aeac4db51
Meghan Spencer <emjayess@gmail.com>
2017-08-22 13:23:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,3 +5,12 @@ function updatePlaylist(playlist,artist,song) {
   console.log(playlist);
   return playlist;
 }
+
+function removeFromPlaylist(playlist,artist) {
+  delete playlist[artist];
+  console.log(playlist);
+  return playlist;
+}
+
+updatePlaylist(playlist,'The Eagles','Desperado');
+removeFromPlaylist(playlist,'Lin Manuel Miranda');
