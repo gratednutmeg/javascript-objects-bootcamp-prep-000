3655da89affcef585835d885145664f8a79f3aa6
Meghan Spencer <emjayess@gmail.com>
2017-08-22 13:23:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,15 +2,10 @@ var playlist = {'Lin Manuel Miranda': 'My Shot'};
 
 function updatePlaylist(playlist,artist,song) {
   playlist[artist] = song;
-  console.log(playlist);
   return playlist;
 }
 
 function removeFromPlaylist(playlist,artist) {
   delete playlist[artist];
-  console.log(playlist);
   return playlist;
 }
-
-updatePlaylist(playlist,'The Eagles','Desperado');
-removeFromPlaylist(playlist,'Lin Manuel Miranda');
